08cde06acf90cee04c7a75c2c7c5c328071d07fe
giancarvalho <giandcarvalho@gmail.com>
2021-11-09 22:30:28 -0300

    added get products and endpoit and respective tests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import createProducts from './controllers/products.js';
+import createProducts from './controllers/createProducts.js';
+import getProducts from './controllers/getProducts.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/products', getProducts);
 app.post('/products', createProducts);
 
 app.get('/health', (req, res) => {
